09a999bbf4412dc165f16a8e790f2fdef46b11e5
ambujsahu81 <Ambuj.sahu1998@gmail.com>
2023-01-01 20:18:43 +0530

    adds the functionality to show content

--- docs/main.js
+++ docs/main.js
@@ -3,6 +3,14 @@ const repoUrl = 'https://api.github.com/repos/ambujsahu81/Notes/contents'
 const noteExtension = '.txt'
 const Readme = 'README.md'
 const backSlash = '/'
+const colorBlue = '#2D67EF'
+const colorPink = '#F48C8C'
+const colorBlack = '#0F0F0F'
+const grey = '#EEEEEE'
+const normalSize = '1rem';
+const bigSize = "1.1rem";
+
+
 const skip = (num) => new Array(num);
 
 const addNote = ( { title, category, path, content } ) => listOfNotes.push( { title, category, path, content } )
@@ -25,8 +33,19 @@ const decodeBase64Str = ( str ) =>  atob( str )
 
 const concat = ( str1, str2=null, str3=null, separator ) => [str1,str2,str3].join(separator).replace(/.$/,'');
 
+
+const hovereffect = ( flag, element ) => {
+    if ( flag ) {
+        element.style.color = `${colorBlue}`
+    } else {
+        element.style.color = `${colorBlack}`
+    }
+
+}
+
+
 const updateDom = () => {
-    
+
     getDomElement('Title').textContent = 'Small collection of usefull notes'
 
     // align content elements
@@ -39,16 +58,22 @@ const updateDom = () => {
 
     let listElement =  getDomElement('List');
     listElement.style.marginLeft = '-4%'
-    const customUL = document.createElement('ul');    
+    const customUL = document.createElement('ul');
+    let index = 0;    
     for ( const note of listOfNotes ) {
         const newLiElement = document.createElement('li');
 
         // update li element
         newLiElement.textContent = note.title;
         newLiElement.style.listStyle = 'auto'
-        newLiElement.style.margin = '1rem'
-        
+        newLiElement.style.margin = '0.5rem'
+        newLiElement.style.padding = '0.5rem'
+        newLiElement.id = `li${index}`
+        newLiElement.style.cursor = 'pointer'
+        newLiElement.setAttribute('onmouseover', `hovereffect( true, li${index} )` )
+        newLiElement.setAttribute('onmouseout', `hovereffect( false, li${index} )` )
 
+ 
         // add a link tag as well
         const newSpanElement = document.createElement('span');
         newSpanElement.innerHTML = '&#8594;'
@@ -58,9 +83,13 @@ const updateDom = () => {
         newLiElement.appendChild(newSpanElement);
 
         customUL.appendChild(newLiElement);
+        index++;
     }
+    customUL.addEventListener('click', (evt) => showContent(evt));
     listElement.appendChild(customUL)
 }
+
+
 
 const parseNotes = ( str ) => {
     if ( !str ) { 
@@ -86,15 +115,61 @@ const parseNotes = ( str ) => {
     }
 }
 
-const parseContent = ( str ) => {
+const parseContent = ( str , index ) => {
     if ( !str ) { 
         return;
     }
+    const contentObj = {};
+    contentObj.headers= [];
+    contentObj.paragraph = [];
 
-    console.log( str );
+
+    section = str.split('--');
+    section.shift()
+
+    // extract the headers and paragraph
+    for ( const content of section ) {
+       tempArray = content.split('\n')
+       const sectionHeader = tempArray[0].trim();
+       contentObj.headers.push(sectionHeader)
+       tempArray.shift()
+       contentObj.paragraph.push(tempArray.join('\n'))
+    }
+    listOfNotes[index].content = contentObj;
+
 }
 
+const showContent = (evt) => {
+    const index = +evt.target.id.replace('li','')
+    const note = listOfNotes[index]
 
+    console.log(note, index)
+    
+    getDomElement('Title').textContent = note.title;
+    getDomElement('List').style.display = 'none'
+    const content = getDomElement('content');
+    content.style.width = '75%'
+    let sectionindex = 0;
+    for( const sectionHeader of note.content.headers ) {
+
+        const header = document.createElement('h3');
+        header.id = `h1${sectionindex}`
+        header.textContent = sectionHeader;
+        header.style.margin = '1rem'
+
+        const paragraph = document.createElement('p');
+        paragraph.id = `P${sectionindex}`
+        console.log(note.content.paragraph[sectionindex])
+        paragraph.innerText = note.content.paragraph[sectionindex]
+        paragraph.style.margin = '1rem'
+        paragraph.style.padding = '1rem'
+
+        paragraph.style.background = grey
+        sectionindex++;
+      content.appendChild(header);
+      content.appendChild(paragraph);
+    }
+}
 
 // fetch calls
 const fetchNotesList = async () => {
@@ -142,8 +217,10 @@ const intialize = async () => {
 
     // fetch content
     const responseContent = await fetchNoteContent();
+    let index = 0;
     for ( const { content } of responseContent ) {
-        parseContent( decodeBase64Str( content ) );
+        parseContent( decodeBase64Str( content ) , index );
+        index++;
     }
     updateDom(); 
 }
